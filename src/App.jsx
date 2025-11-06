5a96578a3c3bd90831278d8c6ede1d827d0f2a02
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-11-06 16:57:22 -0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./index.css";
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-    </>
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold text-center py-8">Meu Portfólio Atualizado</h1>
+    </div>
   );
 }
 
